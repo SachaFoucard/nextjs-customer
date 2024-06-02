1549815d860e6168e638c547c89518100a44ddde
sacha foucard <94567706+SachaFoucard@users.noreply.github.com>
2024-06-02 18:23:00 +0300

    Update route.ts

--- app/api/Works/route.ts
+++ app/api/Works/route.ts
@@ -1,1 +1,18 @@
 
+import WorkShema from "../../models/work";
+import { connectToMongo } from "../../utils/db";
+
+const MONGODB_URI = process.env.MONGODB_URI;
+
+
+export async function GET() {
+    await connectToMongo(MONGODB_URI);
+    try {
+        const works = await WorkShema.find();
+        return Response.json({ works })
+    } catch (error) {
+        return Response.json({ message: 'Internal Server Error' });
+    }
+
+}
+
