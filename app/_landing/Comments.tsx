623706a2e08bb91e1e1cce8c8b7e33dd2ee27381
sacha foucard <94567706+SachaFoucard@users.noreply.github.com>
2024-06-02 18:19:55 +0300

    Update Comments.tsx

--- app/_landing/Comments.tsx
+++ app/_landing/Comments.tsx
@@ -1,6 +1,6 @@
 // ClientSideMarquee.tsx
 'use client'
-import { Component, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import Marquee from "../components/ui/marquee";
 import { Avatar, AvatarImage } from "@radix-ui/react-avatar";
 import { Star } from 'lucide-react';
